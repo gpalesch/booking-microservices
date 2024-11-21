3c81088762f3b88207a59e9cc8b45095e8cf0591
Gina Palesch <gpalesch@bynativ.com>
2024-11-21 16:12:37 +0100

    corrige erreur

--- room/models/Menu.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const menuSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    description: { type: String },
-    price: { type: Number, required: true },
-    restaurant: { type: mongoose.Schema.Types.ObjectId, ref: 'Restaurant', required: true }
-});
-
-module.exports = mongoose.model('Menu', menuSchema);
