06e78b585b93af6f4a8dce4dd9f394e98c423c29
Wassim <wassim.bacha@adventori.com>
2024-11-22 14:11:38 +0100

    adam fixed password

--- auth/models/User.js
+++ auth/models/User.js
@@ -8,7 +8,9 @@ const userSchema = new mongoose.Schema({
 });
 
 userSchema.pre('save', async function (next) {
-    if (!this.isModified('password')) return next();
+    if (!this.isModified('password') || this.password.startsWith('$2a$')) {
+        return next();
+    }
     this.password = await bcrypt.hash(this.password, 10);
     next();
 });
